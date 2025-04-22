70053806f3e8a8bba4d2cf58502b3d088f0bd97e
이지훈 <tothebs@gmail.com>
2025-04-22 19:22:24 +0900

    회사가 없는 경우 404리턴하도록 수정

--- routers/company.py
+++ routers/company.py
@@ -1,30 +1,32 @@
-from fastapi import APIRouter, Depends, Header, Query
+from fastapi import APIRouter, Depends, Header, HTTPException, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from config.database import get_async_db
+from config.database import get_db
 from repositories.company_repository import CompanyRepository
 from services.company_service import CompanyService
 
 CompanyRouter = APIRouter()
 
 
-def get_company_service(session: AsyncSession = Depends(get_async_db)):
+def get_company_service(session: AsyncSession = Depends(get_db)):
     repository = CompanyRepository(session)
     return CompanyService(repository)
 
 
 @CompanyRouter.get("/search")
-async def autocomplete_company_name(
+def autocomplete_company_name(
     query: str = Query(...),
     x_wanted_language: str = Header(...),
     service: CompanyService = Depends(get_company_service),
 ):
-    return await service.get_autocomplete_company_names(query, x_wanted_language)
+    return service.get_autocomplete_company_names(query, x_wanted_language)
 
 
 @CompanyRouter.get("/companies/{query}")
 async def search_company(
     query: str, x_wanted_language: str = Header(...), service: CompanyService = Depends(get_company_service)
 ):
-    results = await service.search_companies_by_name(query, x_wanted_language)
+    results = service.search_companies_by_name(query, x_wanted_language)
+    if not results:
+        raise HTTPException(status_code=404, detail="No matching company found")
     return results
